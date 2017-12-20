42acf6a2ab58dfe788664282dbfa610e3cd5aec7
jason_w <brackman@gmail.com>
2017-12-20 12:45:43 -0800

    day_19 completed.

--- day_19.py
+++ day_19.py
@@ -7,6 +7,13 @@ def get_col(lines, c):
     return column
 
 
+def get_input():
+    with open(r'./input/day_19.txt', 'rt') as handle:
+        test_input = handle.read().split('\n')
+        test_input = [t.ljust(200) for t in test_input]
+    return test_input
+
+
 if __name__ == "__main__":
     test_input = """         |          
          |  +--+    
@@ -16,71 +23,89 @@ if __name__ == "__main__":
          +B-+  +--+ 
     """.split('\n')
 
+    test_input = get_input()
+    steps = 0
     row = 0
     col = test_input[0].find('|')
     letters = list()
     go = 'down'
-    print(row, col)
 
     row_work = False
     col_work = True
     while True:
         if col_work:
             cols = get_col(test_input, col)
+            # print('Col length: ', len(cols))
+
             char = cols[row]
+
             if go == 'undetermined':
-                if row-1 >= 0 and cols[row-1] == '|':
+                if row-1 >= 0 and cols[row-1] != ' ':
+
                     go = 'up'
                     row -= 1
+                    steps += 1
                 else:
                     go = 'down'
                     row += 1
+                    steps += 1
 
+                # print("Direction Change:", go)
             elif char != '+' or char.isalpha():
                 if go == 'down':
                     row += 1
+                    steps += 1
                 if go == 'up':
                     row -= 1
+                    steps += 1
                 if char.isalpha():
                     letters.append(char)
-                    print(row, col, char)
+                    print(go, row, col, letters, char)
+                    if cols[row] == ' ':
+                        print("Done!")
+                        break
 
             elif char == '+':
                 col_work = False
                 row_work = True
                 go = 'undetermined'
 
-            print(go, row, col, letters)
-
         elif row_work:
             rows = test_input[row]
+
             char = rows[col]
+
             if go == 'undetermined':
-                if col-1 >= 0 and rows[col-1] == '-':
+                if col-1 >= 0 and rows[col-1] != ' ':
                     go = 'left'
                     col -= 1
+                    steps += 1
                 else:
                     go = 'right'
                     col += 1
+                    steps += 1
+                # print("Direction Change:", go)
 
             elif rows[col] != '+' or rows[col].isalpha():
                 if go == 'right':
                     col += 1
-                if go == 'left':
+                    steps += 1
+                else:
                     col -= 1
+                    steps += 1
                 if char.isalpha():
                     letters.append(char)
+                    print(go, row, col, letters, char)
+                    if rows[col] == ' ':
+                        print("Done!")
+                        break
 
             elif rows[col] == '+':
                 col_work = True
                 row_work = False
                 go = 'undetermined'
 
-            print(go, row, col, letters)
-
-
-
-
-
+    print('Part One: ', "".join(letters)) # EPYDUXANIT
+    print('Part Two: ', steps)
 
 
