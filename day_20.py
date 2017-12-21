5ece2e8933d63fb9e9a4aad92d1ce727edd9faba
jasonbrackman <brackman@gmail.com>
2017-12-20 22:43:08 -0800

    Day 20 completed

--- day_20.py
+++ day_20.py
@@ -1,41 +1,104 @@
+
+import itertools
+
+import collections
+
+
+class Particle:
+
+    def __init__(self, px, py, pz,
+                       vx, vy, vz,
+                       ax, ay, az):
+        self.posX = int(px)
+        self.posY = int(py)
+        self.posZ = int(pz)
+
+        self.velX = int(vx)
+        self.velY = int(vy)
+        self.velZ = int(vz)
+
+        self.accX = int(ax)
+        self.accY = int(ay)
+        self.accZ = int(az)
+
+    def get_absolute_pos(self):
+        return sum([abs(self.posX), abs(self.posY), abs(self.posZ)])
+
+    def get_position(self):
+        return self.posX, self.posY, self.posZ
+
+
 def get_buffer():
     with open(r'./input/day_20.txt', 'rt') as handle:
         lines = handle.readlines()
         lines = [line.replace("'", '').split(' ') for line in lines]
 
-
     buffer = list()
     for line in lines:
-        print(line)
-        1/0
+
+        pva = {}
         for item in line:
-            print(item)
-            1/0
-            parts = item.replace("<", "").replace("'", "").replace(">", "").strip()
-            p = parts[0]
-            print('parts:', parts)
-            1/0
+            type_, xyz = item.split('=')
+            x, y, z, *_ = (xyz.replace('<', '').replace('>', '').split(','))
+            pva[type_] = [x, y, z]
 
-    # for particle in buffer:
-    #     print(particle)
-    return lines
+        buffer.append(Particle(*pva['p'], *pva['v'], *pva['a']))
 
-def update_particles(buffer):
-    # increase the x velocity by the x acceleration
-    # increase the y velocity by teh y acceleration
-    # increase the z velocity by the z acceleration
-    # increase the x position by the x velocity
-    # increase the y position by the y velocity
-    # increase the z position by the z velocity
     return buffer
 
 
+def update_particles(buffer):
+    for particle in buffer:
+        # increase the x velocity by the x acceleration
+        particle.velX += particle.accX
+
+        # increase the y velocity by teh y acceleration
+        particle.velY += particle.accY
+
+        # increase the z velocity by the z acceleration
+        particle.velZ += particle.accZ
+
+        # increase the x position by the x velocity
+        particle.posX += particle.velX
+
+        # increase the y position by the y velocity
+        particle.posY += particle.velY
+
+        # increase the z position by the z velocity
+        particle.posZ += particle.velZ
+
+    return buffer
+
+
+def part_one():
+    # for part one
+    d = collections.defaultdict(int)
+    buffer = get_buffer()
+    for tick in range(len(buffer)):
+        buffer = update_particles(buffer)
+        positions = [particle.get_absolute_pos() for particle in buffer]
+        d[positions.index(min(positions))] += 1
+    key = None
+    highest = 0
+    for k, v in d.items():
+        if v > highest:
+            highest = v
+            key = k
+    print("Part One: ", key)  # 125
+
 
 if __name__ == "__main__":
-    # p = position(x, y, z)
-    # v = velocity(x, y, z)
-    # a = acceleration(x, y, z)
-    # format: particle(p, v, a)
+    part_one()
+
     buffer = get_buffer()
-    pass
+    print(len(buffer))
+    for tick in range(len(buffer)):
+        buffer = update_particles(buffer)
+
+        positions = collections.Counter([particle.get_position() for particle in buffer])
+
+        buffer = [b for b in buffer if positions[b.get_position()] == 1]
+
+    print("Part Two: ", len(buffer))  # 461
+
 
